15d85c99fa9cba30749717b50e2bba0b85aea6dd
lander.corral.ruiz@zabalburu.com <lander.corral.ruiz@zabalburu.com>
2020-10-19 10:54:40 +0200

    Ejercicio 1 subido

---
+++ src/com/ipartek/ejercicio/estructurasRepetitivas/Ejercicio1.java
@@ -0,0 +1,21 @@
+package com.ipartek.ejercicio.estructurasRepetitivas;
+
+/**
+ * Realizar un ejercicio que nos muestre la tabla de multiplicar del 6
+ * 
+ * @version 1.0
+ * @author LANDER
+ *
+ */
+
+public class Ejercicio1 {
+
+	static final int MULTIPLICADOR = 6;
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++) {
+			System.out.printf("%sx%s=%s\n", MULTIPLICADOR, i, MULTIPLICADOR * i);
+		}
+	}
+
+}
